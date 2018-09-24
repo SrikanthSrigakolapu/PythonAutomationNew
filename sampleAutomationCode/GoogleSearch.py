6381108258022aba7dfb064991088884d24ef7c2
ssrigakolapu <srikanth.srigakolapu@gmail.com>
2018-09-24 11:21:28 +0530

    this commit contains the linux drivers and sample automation code to invoke google

---
+++ sampleAutomationCode/GoogleSearch.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+import os
+from selenium.webdriver.common.keys import Keys
+parentdir=os.path.dirname(os.path.abspath(__file__))
+
+print(os.path.dirname(parentdir)+"\WindowsDrivers\chromedriver.exe")
+#invoking the chrome instance
+driverchrome=webdriver.Chrome(executable_path=os.path.dirname(parentdir)+"\WindowsDrivers\chromedriver.exe")
+
+#opening the url
+driverchrome.get("https://www.google.com")
+
+#verify the title
+assert "Google" in driverchrome.title
+
+print("process is done")
+
+driverchrome.close()
