f8646bf545eee35df319f157c436baf5955c186c
ssrigakolapu <srikanth.srigakolapu@gmail.com>
2018-09-24 10:02:28 +0530

    added two more statements to verify the commit

--- sampleAutomation.py
+++ sampleAutomation.py
@@ -1,3 +1,5 @@
 print("this file is in GIT I suppose")
 print("I have modified this code from my work station")
 print("lets see whether it is updated in the GITHUB")
+print("whats happening")
+print("verify")
