dad4d93f4cb892c26b967b256373706347efe5c5
ssrigakolapu <srikanth.srigakolapu@gmail.com>
2018-09-25 11:54:34 +0530

    google search file updated with sample screen shot save

--- sampleAutomationCode/GoogleSearch.py
+++ sampleAutomationCode/GoogleSearch.py
@@ -1,19 +1,31 @@
 from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
 import os
 from selenium.webdriver.common.keys import Keys
 
-//below is the one for getting the current directory
+#below is the one for getting the current directory
 parentdir=os.path.dirname(os.path.abspath(__file__))
 
 print(os.path.dirname(parentdir)+"\WindowsDrivers\chromedriver.exe")
 #invoking the chrome instance
 driverchrome=webdriver.Chrome(executable_path=os.path.dirname(parentdir)+"\WindowsDrivers\chromedriver.exe")
 
+print("Chrome invoked successfully")
+
 #opening the url
 driverchrome.get("https://www.google.com")
 
 #verify the title
 assert "Google" in driverchrome.title
+
+print("Done hitting the url")
+driverchrome.save_screenshot('sample.png')
+searchElement=driverchrome.find_element_by_class_name('gsfi')
+searchElement.send_keys("This is Srikanth")
+searchElement.submit()
+
+time.sleep(10)
 
 print("process is done")
 
