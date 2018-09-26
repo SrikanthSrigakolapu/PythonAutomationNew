41609c3836c28e439b40d6ee8436fbff426b61f1
ssrigakolapu <srikanth.srigakolapu@gmail.com>
2018-09-26 11:49:15 +0530

    SeleniumEasy test website scenario 1 execution

--- ProjectSeleniumEasy/SC1_SingleInputField.py
+++ ProjectSeleniumEasy/SC1_SingleInputField.py
@@ -10,15 +10,41 @@ class SingleInputField(unittest.TestCase):
         #initiate the Chrome instance
         parentpath=os.path.dirname(os.path.abspath(__file__))
         self.chromeDriver=webdriver.Chrome(executable_path=os.path.dirname(parentpath)+"\WindowsDrivers\chromedriver.exe")
+        self.chromeDriver.maximize_window()
 
     def test_OpenSingleFormDemo(self):
         chromedriver=self.chromeDriver
-        chromedriver.get("httos://seleniumeasy.com/test/")
-        self.assertEqual('Selenium Easy',chromedriver.title)
+        chromedriver.get("https://seleniumeasy.com/test/")
+        #self.assertEqual("Selenium Easy"chromedriver.title)
 
         #TODO: right now I am using print statements need to look for logging option in python
         print("Success: Opened the url in chrome")
 
+        inputFromsElem=chromedriver.find_element_by_xpath("//a[normalize-space()=\"Input Forms\"]/b[@class=\"caret\"]")
+        inputFromsElem.click()
+
+        chromedriver.save_screenshot("InputFormsClick.png")
+        print("SuccessFully clicked the InputFormsClick")
+
+        SFDelem=chromedriver.find_element_by_xpath("//ul[@class=\"dropdown-menu\"]/li/a[text()=\"Simple Form Demo\"]")
+        SFDelem.click()
+        chromedriver.save_screenshot("SimpleFormDemoPage.png")
+
+        print("entered the Simple Form Demo page")
+
+    #def test_EnterSingleInput(self):
+        chromedriver=self.chromeDriver
+        singleInputFieldElement=chromedriver.find_element_by_xpath("//input[@id=\"user-message\"]")
+        singleInputFieldElement.send_keys("Hello this is sentinel")
+
+        showMessageButton=chromedriver.find_element_by_xpath("//button[text()=\"Show Message\"]")
+        showMessageButton.click()
+        chromedriver.save_screenshot("ShowMessage.png")
 
 
+    def tearDown(self):
+        self.chromeDriver.close()
 
+
+if __name__ == '__main__':
+    unittest.main()
