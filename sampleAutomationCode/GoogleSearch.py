7d921d20271ae1ef128f35bdb83867a235135402
Srikanth Srigakolapu <31268084+SrikanthSrigakolapu@users.noreply.github.com>
2019-02-07 16:33:36 +0530

    Update GoogleSearch.py

--- sampleAutomationCode/GoogleSearch.py
+++ sampleAutomationCode/GoogleSearch.py
@@ -9,7 +9,7 @@ parentdir=os.path.dirname(os.path.abspath(__file__))
 
 print(os.path.dirname(parentdir)+"\LinuxDrivers\chromedriver.exe")
 #invoking the chrome instance
-driverchrome=webdriver.Chrome(executable_path=os.path.dirname(parentdir)+"\LinuxDrivers\chromedriver.exe")
+driverchrome=webdriver.Chrome(executable_path=os.path.dirname(parentdir)+"\LinuxDrivers\chromedriver")
 
 print("Chrome invoked successfully")
 
